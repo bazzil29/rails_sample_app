f0b02b83129282538cf13e173e376b3001fd06e1
ppbra <kaka020998@gmail.com>
2019-07-24 11:25:03 +0700

    chap 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home'
 
   get  '/help',    to: 'static_pages#help' 
@@ -15,6 +17,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :account_activations , only: [:edit]
+  resources :password_resets , only: [:edit, :new, :create, :update]
   
 end
 
